7929a5ace4b66d99efdb17fd033f1632904c0dd7
Edgars Eizvertins <edgars.eizvertins@gmail.com>
2021-10-22 21:50:40 +0300

    remove comments

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
 	print("start")
 	main()
 
-	#http://history.openweathermap.org/data/2.5/history/city?q=Riga,LV&type=hour&start=1369728000&end=1369789200&appid=7edbb2dc4357f686916a690481f6b72d
+
