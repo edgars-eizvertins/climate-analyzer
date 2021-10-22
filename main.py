a1ff5f234f8e99120429651500126facce0076d0
Edgars Eizvertins <edgars.eizvertins@gmail.com>
2021-10-22 21:10:55 +0300

    Add main file

---
+++ main.py
@@ -0,0 +1,9 @@
+
+def main():
+	print("main")
+
+if __name__ == "__main__":
+	print("start")
+	main()
+
+	#http://history.openweathermap.org/data/2.5/history/city?q=Riga,LV&type=hour&start=1369728000&end=1369789200&appid=7edbb2dc4357f686916a690481f6b72d
